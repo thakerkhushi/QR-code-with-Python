b8b82fd23b6ea712f59b0a91a696f41f48d034c0
khushi thaker <104619860+thakerkhushi@users.noreply.github.com>
2022-07-20 17:17:32 +0530

    Create QRcodeGenrater.py

---
+++ QRcodeGenrater.py
@@ -0,0 +1,15 @@
+import qrcode
+import image
+
+qr = qrcode.QRCode(
+    version=15,
+    box_size=4,
+    border=5
+)
+
+data = "https://zoophlist.wordpress.com/"
+
+qr.add_data(data)
+qr.make(fit=True)
+image = qr.make_image(fill="black", back_color="white")
+image.save("test.png")
